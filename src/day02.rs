9d26a0789e2943442c9a3e0cafab8498f80a575d
Jort van Waes <jortvanwaes@protonmail.com>
2023-12-02 18:25:35 +0100

    Update day02.rs, just an idea

--- src/day02.rs
+++ src/day02.rs
@@ -1,3 +1,6 @@
+use aocf::Aoc;
+use regex::Regex;
+
 fn main() {
-    println!("Hello, World! from src/day02.rs!"); 
+    let sum = Aoc::new().year(Some(2023)).day(Some(2)).init().unwrap().get_input(false).split("\n").collect::<Vec<&str>>().iter().filter(|line|Regex::new(r"(\d+)\sblue").unwrap().captures_iter(line).any(|cap|&cap[1].parse::<u32> > 12)).map(|line|Regex::new(r"Game\s(\d+)").unwrap() //TODO
 }

85fdc4ee8c7194f28c4501b53e8f3c36e2edc162
Jort vanWaes <JRR.vanWaes@student.han.nl>
2023-12-02 13:43:01 +0000

    Added the (future) 25 days of Advent of Code 2023 to cargo.toml as separate binaries

--- src/day01.rs
+++ src/day01.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("Hello world!");
+    for i in 1..=25 {
+        //TODO: Your code here
+        println!("[[bin]]");
+        println!("name = \"day{:02}\"", i);
+        println!("path = \"src/day{:02}.rs\"", i);
+        println!();
+    }
 }

b1ea24140ea12764ea6f7f209bcaca199b81ddf3
Jort van Waes <jortvanwaes@protonmail.com>
2023-12-02 17:22:06 +0100

    Update day01.rs, added unwrap to chars calls

--- src/day01.rs
+++ src/day01.rs
@@ -1,5 +1,5 @@
 use aocf::Aoc;
 
 fn main() {
-    let sum = Aoc::new().year(Some(2023)).day(Some(1)).init().unwrap().get_input(false).split("\n").collect::<Vec<&str>>().iter().map(|&line| line.chars().filter(|c|c.is_digit()).collect::<String>()).map(|&line|format!("{}{}",line.chars().next(),line.chars().last()).parse::<u32>().unwrap()).sum();
+    let sum = Aoc::new().year(Some(2023)).day(Some(1)).init().unwrap().get_input(false).split("\n").collect::<Vec<&str>>().iter().map(|&line| line.chars().filter(|c|c.is_digit()).collect::<String>()).map(|&line|format!("{}{}",line.chars().next().unwrap(),line.chars().last().unwrap()).parse::<u32>().unwrap()).sum();
 }

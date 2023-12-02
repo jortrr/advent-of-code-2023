043d9db6e66abcfe6e0e3fbc557550ca540153bc
Jort vanWaes <JRR.vanWaes@student.han.nl>
2023-12-02 19:15:03 +0000

    Solved day 1 part 1

--- src/day01.rs
+++ src/day01.rs
@@ -1,5 +1,6 @@
 use aocf::Aoc;
 
 fn main() {
-    let sum = Aoc::new().year(Some(2023)).day(Some(1)).init().unwrap().get_input(false).split("\n").collect::<Vec<&str>>().iter().map(|&line| line.chars().filter(|c|c.is_digit()).collect::<String>()).map(|&line|format!("{}{}",line.chars().next().unwrap(),line.chars().last().unwrap()).parse::<u32>().unwrap()).sum();
+    let sum: u32 = Aoc::new().year(Some(2023)).day(Some(1)).init().unwrap().get_input(false).unwrap().split("\n").collect::<Vec<&str>>().iter().map(|line| line.chars().filter(|c| c.is_digit(10)).collect::<String>()).filter(|line| !line.is_empty()).map(|line| format!("{}{}", line.chars().next().unwrap(), line.chars().last().unwrap()).parse::<u32>().unwrap()).sum();
+    dbg!(sum);
 }

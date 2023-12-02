b4dc6dcf7c3d565866ff88e6c9a09f99cd4e7be2
Jort vanWaes <JRR.vanWaes@student.han.nl>
2023-12-02 14:37:44 +0000

    Added Advent of Code Fetch Rust snippet, started day 01

--- src/day01.rs
+++ src/day01.rs
@@ -1,10 +1,13 @@
+use aocf::Aoc;
+
 fn main() {
-    println!("Hello world!");
-    for i in 1..=25 {
-        //TODO: Your code here
-        println!("[[bin]]");
-        println!("name = \"day{:02}\"", i);
-        println!("path = \"src/day{:02}.rs\"", i);
-        println!();
+    let mut aoc = Aoc::new().year(Some(2023)).day(Some(1)).init().unwrap();
+
+    // Get input data (don't force)
+    let input = aoc.get_input(false);
+
+    if let Ok(i) = input {
+        println!("Input for day {:02}:\n", aoc.day.unwrap());
+        println!("{}", i);
     }
 }

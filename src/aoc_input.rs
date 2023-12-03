ac48857bd2f3f38d0f804a6e2c15a56f444c7a8a
Jort vanWaes <JRR.vanWaes@student.han.nl>
2023-12-03 10:21:56 +0000

    Day 1: Made the solution ~200 characters shorter, removed repetitive code, removed another line

---
+++ src/aoc_input.rs
@@ -0,0 +1,5 @@
+use aocf::Aoc;
+
+pub fn get(year: i32, day: u32) -> Vec<String> {
+    Aoc::new().year(Some(year)).day(Some(day)).init().unwrap().get_input(false).unwrap().split("\n").map(str::to_string).collect::<Vec<String>>()
+}
--- src/day03.rs
+++ src/day03.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, World! from src/day03.rs!"); 
+    println!("Hello, World! from src/day03.rs!");
+    //let tuple: (u32, u32, u32) = [1, 2, 3].into_iter().map(|x| x * 2).collect();
+
+    //dbg!(tuple);
+
+    let x = ["Hel2lo", "World2"]
+        .iter()
+        .map(|line| line.chars().filter(|c| c.is_digit(10)).collect::<String>())
+        .collect::<Vec<String>>();
+    dbg!(x);
 }

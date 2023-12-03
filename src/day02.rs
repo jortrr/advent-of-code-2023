e69ed82bd839b7b5867e9bf5a19a893ce53c1494
Jort vanWaes <JRR.vanWaes@student.han.nl>
2023-12-03 12:48:09 +0000

    Turned day2 into a oneliner

--- src/day02.rs
+++ src/day02.rs
@@ -1,34 +1,3 @@
-use regex::Regex;
-
 fn main() {
-    let x: u32 = aoc_input::get(2023, 2)
-        .into_iter()
-        .filter(|line| {
-            Regex::new(r"(\d+)\s(\w+)")
-                .unwrap()
-                .captures_iter(&line)
-                .all(|cap| {
-                    let value = *&cap[1].parse::<u32>().unwrap();
-                    let color = &cap[2];
-                    (color == "red" && value <= 12)
-                        || (color == "green" && value <= 13)
-                        || (color == "blue" && value <= 14)
-                })
-        })
-        .filter(|line| !line.is_empty())
-        .map(|line| {
-            (
-                line.clone(),
-                Regex::new(r"Game\s(\d+)")
-                    .unwrap()
-                    .captures_iter(&line)
-                    .next()
-                    .unwrap()[1]
-                    .parse::<u32>()
-                    .unwrap(),
-            )
-        })
-        .map(|(_, id)| id)
-        .sum();
-    dbg!(x);
+    println!("Solution: {}", aoc_input::get(2023, 2).into_iter().filter(|line| regex::Regex::new(r"(\d+)\s(\w+)").unwrap().captures_iter(&line).map(|cap| (*&cap[1].parse::<u32>().unwrap(), String::from(&cap[2]))).all(|(value, color)| (color == "red" && value <= 12) || (color == "green" && value <= 13) || (color == "blue" && value <= 14))).filter(|line| !line.is_empty()).map(|line| (line.clone(), regex::Regex::new(r"Game\s(\d+)").unwrap().captures_iter(&line).next().unwrap()[1].parse::<u32>().unwrap())).map(|(_, id)| id).sum::<u32>());
 }

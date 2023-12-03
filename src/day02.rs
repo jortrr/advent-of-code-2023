2187f4e68d3f7d101ccadb575feeb8b2f7d681fa
Jort vanWaes <JRR.vanWaes@student.han.nl>
2023-12-03 12:41:39 +0000

    Solved day 2 part 1

--- src/day02.rs
+++ src/day02.rs
@@ -1,6 +1,34 @@
-use aocf::Aoc;
 use regex::Regex;
 
 fn main() {
-    let sum = Aoc::new().year(Some(2023)).day(Some(2)).init().unwrap().get_input(false).split("\n").collect::<Vec<&str>>().iter().filter(|line|Regex::new(r"(\d+)\sblue").unwrap().captures_iter(line).any(|cap|&cap[1].parse::<u32> > 12)).map(|line|Regex::new(r"Game\s(\d+)").unwrap() //TODO
+    let x: u32 = aoc_input::get(2023, 2)
+        .into_iter()
+        .filter(|line| {
+            Regex::new(r"(\d+)\s(\w+)")
+                .unwrap()
+                .captures_iter(&line)
+                .all(|cap| {
+                    let value = *&cap[1].parse::<u32>().unwrap();
+                    let color = &cap[2];
+                    (color == "red" && value <= 12)
+                        || (color == "green" && value <= 13)
+                        || (color == "blue" && value <= 14)
+                })
+        })
+        .filter(|line| !line.is_empty())
+        .map(|line| {
+            (
+                line.clone(),
+                Regex::new(r"Game\s(\d+)")
+                    .unwrap()
+                    .captures_iter(&line)
+                    .next()
+                    .unwrap()[1]
+                    .parse::<u32>()
+                    .unwrap(),
+            )
+        })
+        .map(|(_, id)| id)
+        .sum();
+    dbg!(x);
 }

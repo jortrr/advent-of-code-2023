63d939243e358d326a2d7570e3a8fe80a100b66a
Jort van Waes <jort.van.waes@inspiro.nl>
2023-12-04 12:41:51 +0000

    Solved day 04 part 1

--- src/day04.rs
+++ src/day04.rs
@@ -1,3 +1,44 @@
+mod regex_captures;
+
 fn main() {
-    println!("Hello, World! from src/day04.rs!"); 
+    let x: u32 = aoc_input::get(2023, 4)
+        .iter()
+        .filter(|s| !s.is_empty())
+        .map(|line| line.split(|c| c == ':' || c == '|').collect::<Vec<&str>>())
+        .map(|parts| {
+            (
+                parts[0],
+                vec![parts[1], parts[2]]
+                    .iter()
+                    .map(|s| {
+                        s.split(' ')
+                            .filter(|w| !w.is_empty())
+                            .map(|w| String::from(w))
+                            .collect::<Vec<String>>()
+                    })
+                    .collect::<Vec<_>>(),
+            )
+        })
+        .map(|(card, nums)| {
+            (
+                card,
+                nums[0].clone(),
+                nums[1].clone(),
+                nums[0]
+                    .iter()
+                    .filter(|value| nums[1].contains(value))
+                    .map(|s| s.parse::<u32>().unwrap())
+                    .collect::<Vec<u32>>(),
+            )
+        })
+        .map(|(card, win, has, wins)| {
+            if wins.len() == 0 {
+                0
+            } else {
+                (2 as u32).pow((wins.len() - 1) as u32)
+            }
+        })
+        .sum();
+    //.for_each(|f| println!("{:?}", f));
+    dbg!(x);
 }

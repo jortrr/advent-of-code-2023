7eaefb68869cf630f722c8f2d49896a48f7abc83
Jort van Waes <jort.van.waes@inspiro.nl>
2023-12-04 12:43:20 +0000

    Turned day 4 part 1 into a oneliner

--- src/day04.rs
+++ src/day04.rs
@@ -1,44 +1,5 @@
 mod regex_captures;
 
 fn main() {
-    let x: u32 = aoc_input::get(2023, 4)
-        .iter()
-        .filter(|s| !s.is_empty())
-        .map(|line| line.split(|c| c == ':' || c == '|').collect::<Vec<&str>>())
-        .map(|parts| {
-            (
-                parts[0],
-                vec![parts[1], parts[2]]
-                    .iter()
-                    .map(|s| {
-                        s.split(' ')
-                            .filter(|w| !w.is_empty())
-                            .map(|w| String::from(w))
-                            .collect::<Vec<String>>()
-                    })
-                    .collect::<Vec<_>>(),
-            )
-        })
-        .map(|(card, nums)| {
-            (
-                card,
-                nums[0].clone(),
-                nums[1].clone(),
-                nums[0]
-                    .iter()
-                    .filter(|value| nums[1].contains(value))
-                    .map(|s| s.parse::<u32>().unwrap())
-                    .collect::<Vec<u32>>(),
-            )
-        })
-        .map(|(card, win, has, wins)| {
-            if wins.len() == 0 {
-                0
-            } else {
-                (2 as u32).pow((wins.len() - 1) as u32)
-            }
-        })
-        .sum();
-    //.for_each(|f| println!("{:?}", f));
-    dbg!(x);
+    println!("Solution: {:?}", aoc_input::get(2023, 4).iter().filter(|s| !s.is_empty()).map(|line| line.split(|c| c == ':' || c == '|').collect::<Vec<&str>>()).map(|parts| (parts[0], vec![parts[1], parts[2]].iter().map(|s| s.split(' ').filter(|w| !w.is_empty()).map(|w| String::from(w)).collect::<Vec<String>>()).collect::<Vec<_>>())).map(|(card, nums)| (card, nums[0].clone(), nums[1].clone(), nums[0].iter().filter(|value| nums[1].contains(value)).map(|s| s.parse::<u32>().unwrap()).collect::<Vec<u32>>())).map(|(_, _, _, wins)| if wins.len() == 0 { 0 } else { (2 as u32).pow((wins.len() - 1) as u32) }).sum::<u32>());
 }

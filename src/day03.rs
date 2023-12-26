e84b4afe6d5042d14ea6baf86c101dac021f5fbb
Jort vanWaes <JRR.vanWaes@student.han.nl>
2023-12-26 22:09:24 +0000

    Completed D03P01

--- src/day03.rs
+++ src/day03.rs
@@ -1,12 +1,55 @@
+mod regex_captures;
+
 fn main() {
-    println!("Hello, World! from src/day03.rs!");
-    //let tuple: (u32, u32, u32) = [1, 2, 3].into_iter().map(|x| x * 2).collect();
-
-    //dbg!(tuple);
-
-    let x = ["Hel2lo", "World2"]
+    let a = [vec![String::from("Begin")], aoc_input::get(2023, 3)].concat();
+    //dbg!(&a);
+    let b: Vec<_> = a
+        .windows(3)
+        .map(|window| (window, regex_captures::get(r"(\d+)", &window[1])))
+        .collect();
+    //dbg!(&b);
+    let c: Vec<_> = b
         .iter()
-        .map(|line| line.chars().filter(|c| c.is_digit(10)).collect::<String>())
-        .collect::<Vec<String>>();
-    dbg!(x);
+        .map(|(windows, captures)| {
+            (
+                windows,
+                captures
+                    .iter()
+                    .map(|cap| cap.get(1).unwrap())
+                    .collect::<Vec<_>>(),
+            )
+        })
+        .collect();
+    //dbg!(&c);
+    let d: Vec<_> = c
+        .iter()
+        .map(|(windows, captures)| {
+            (
+                windows,
+                captures
+                    .iter()
+                    .filter(|m| {
+                        for i in m.start()..(m.end() + 2) {
+                            for j in 0..3 {
+                                let index = if i > 0 { i - 1 } else { i };
+                                let token = windows[j].chars().nth(index).unwrap_or('.');
+                                println!("i: {}, j: {}, token: {}", i, j, token);
+                                if !token.is_digit(10) && token != '.' {
+                                    return true;
+                                }
+                            }
+                        }
+                        false
+                    })
+                    .map(|m| m.as_str().parse::<u32>().unwrap())
+                    .collect::<Vec<_>>(),
+            )
+        })
+        .collect();
+    dbg!(&d);
+    let e = d
+        .iter()
+        .map(|(_, numbers)| numbers.iter().sum::<u32>())
+        .sum::<u32>();
+    dbg!(e);
 }

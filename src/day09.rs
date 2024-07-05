b481cd6e0702379b629147d8c087d63c7e392d9c
Jort van Waes <jortvanwaes@protonmail.com>
2024-07-05 15:40:11 +0200

    d09p02

--- src/day09.rs
+++ src/day09.rs
@@ -1,7 +1,7 @@
-use std::fmt::write;
+use std::collections::VecDeque;
 
 type Int = i32;
-type Sequence = Vec<Int>;
+type Sequence = VecDeque<Int>;
 
 struct History {
     sequences: Vec<Sequence>,
@@ -18,7 +18,7 @@ impl History {
         }
     }
 
-    fn extrapolate(&mut self) {
+    fn find_differences(&mut self) {
         loop {
             let last_sequence = self.sequences.last().unwrap();
             if last_sequence.iter().all(|x| *x == 0) {
@@ -27,20 +27,36 @@ impl History {
             let differences = History::get_differences(last_sequence);
             self.sequences.push(differences);
         }
+    }
+
+    fn extrapolate(&mut self) {
         let n = self.sequences.len() - 1;
         for i in (0..n).rev() {
             if i == n {
-                self.sequences[i].push(0);
+                self.sequences[i].push_back(0);
             } else {
                 let extrapolation =
-                    self.sequences[i + 1].last().unwrap() + self.sequences[i].last().unwrap();
-                self.sequences[i].push(extrapolation);
+                    self.sequences[i + 1].back().unwrap() + self.sequences[i].back().unwrap();
+                self.sequences[i].push_back(extrapolation);
+            }
+        }
+    }
+
+    fn extrapolate_first_value(&mut self) {
+        let n = self.sequences.len() - 1;
+        for i in (0..n).rev() {
+            if i == n {
+                self.sequences[i].push_front(0);
+            } else {
+                let extrapolation =
+                    self.sequences[i].front().unwrap() - self.sequences[i + 1].front().unwrap();
+                self.sequences[i].push_front(extrapolation);
             }
         }
     }
 
     fn get_differences(input_sequence: &Sequence) -> Sequence {
-        input_sequence
+        Vec::from(input_sequence.clone())
             .windows(2)
             .map(|pair| pair[1] - pair[0])
             .collect()
@@ -79,15 +95,24 @@ impl OASIS {
     }
 
     fn extrapolate_histories(&mut self) {
-        self.histories
-            .iter_mut()
-            .for_each(|history| history.extrapolate());
+        self.histories.iter_mut().for_each(|history| {
+            history.find_differences();
+            history.extrapolate();
+            history.extrapolate_first_value()
+        });
     }
 
     fn sum_of_histories_last_values(&self) -> Int {
         self.histories
             .iter()
-            .map(|h| h.sequences.first().unwrap().last().unwrap())
+            .map(|h| h.sequences.first().unwrap().back().unwrap())
+            .sum()
+    }
+
+    fn sum_of_histories_first_values(&self) -> Int {
+        self.histories
+            .iter()
+            .map(|h| h.sequences.first().unwrap().front().unwrap())
             .sum()
     }
 }
@@ -114,4 +139,6 @@ fn main() {
     oasis.extrapolate_histories();
     let sum_of_histories_last_values = oasis.sum_of_histories_last_values();
     dbg!(sum_of_histories_last_values);
+    let sum_of_histories_first_values = oasis.sum_of_histories_first_values();
+    dbg!(sum_of_histories_first_values);
 }

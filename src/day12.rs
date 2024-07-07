f7318eef007349d551788e63c409cf1b4972f93f
Jort van Waes <jortvanwaes@protonmail.com>
2024-07-07 23:30:31 +0200

    d12p2 - Part 2 still does not work, need to think more

--- src/day12.rs
+++ src/day12.rs
@@ -1,11 +1,22 @@
 type Int = i32;
 
-static RUN_PART_1: bool = false;
+static RUN_PART_1: bool = true;
+static RUN_PART_2: bool = false;
+static DEBUG: bool = false;
+
+macro_rules! debug_print {
+    ($($arg:tt)*) => {
+        if DEBUG {
+            use std::io::{self, Write};
+            println!($($arg)*);
+            io::stdout().flush().unwrap();
+        }
+    };
+}
 
 struct Record {
     springs: String,
     damaged_spring_groups: Vec<Int>,
-    valid_arrangements: Vec<String>,
 }
 
 impl Record {
@@ -18,64 +29,20 @@ impl Record {
             .split(",")
             .map(|v| v.parse::<Int>().unwrap())
             .collect();
-        let mut record = Record {
+        Record {
             springs,
             damaged_spring_groups,
-            valid_arrangements: Vec::new(),
-        };
-        record.generate_arrangements();
-        record
-    }
-
-    fn valid(input: &str, damaged_spring_groups: &Vec<Int>) -> bool {
-        input
-            .split('.')
-            .filter(|s| !s.is_empty())
-            .map(|s| s.len() as Int)
-            .collect::<Vec<Int>>()
-            == *damaged_spring_groups
-    }
-
-    fn generate(input: &str) -> Vec<String> {
-        let mut ready_strings = Vec::new();
-        let mut unready_strings = vec![input.to_string()];
-        while !unready_strings.is_empty() {
-            let current_string = unready_strings.pop().unwrap();
-            if current_string.contains("?") {
-                let a = current_string.replacen("?", ".", 1);
-                let b = current_string.replacen("?", "#", 1);
-                unready_strings.push(a);
-                unready_strings.push(b);
-            } else {
-                ready_strings.push(current_string);
-            }
         }
-        ready_strings
     }
 
-    fn generate_arrangements(&mut self) {
-        self.valid_arrangements = Record::generate(&self.springs)
-            .iter()
-            .filter(|s| Record::valid(s, &self.damaged_spring_groups))
-            .cloned()
-            .collect();
-    }
-
-    fn sum_arrangements(input: Vec<String>, expand: bool) -> Int {
-        input
-            .iter()
-            .filter(|&s| !s.is_empty())
-            .map(|s| Record::from_string(s))
-            .map(|r| if expand { r.expand() } else { r })
-            .map(|r| r.valid_arrangements.len() as Int)
-            .sum()
+    fn solve(&self) -> Int {
+        solve(&self.springs, &self.damaged_spring_groups, 0, "")
     }
 
     fn expand(&self) -> Record {
         let mut result = Record {
             springs: String::new(),
             damaged_spring_groups: Vec::new(),
-            valid_arrangements: Vec::new(),
         };
         let springs = self.springs.clone();
         let groups = self.damaged_spring_groups.clone();
@@ -83,14 +50,23 @@ impl Record {
             result.springs.push_str(&format!("?{}", springs));
             result.damaged_spring_groups.append(&mut groups.clone());
         }
-        result.generate_arrangements();
         result
     }
 
     fn test(input: &str, expected_arrangements: Int) {
-        let mut record = Record::from_string(input);
-        record.generate_arrangements();
-        let actual_arrangements = record.valid_arrangements.len() as Int;
+        let record = Record::from_string(input);
+        let actual_arrangements = solve(&record.springs, &record.damaged_spring_groups, 0, "");
+        dbg!((input, actual_arrangements));
+        assert_eq!(
+            expected_arrangements, actual_arrangements,
+            "Test case failed: this value should always equal '{}'.",
+            expected_arrangements
+        );
+    }
+
+    fn test_expanded(input: &str, expected_arrangements: Int) {
+        let record = Record::from_string(input).expand();
+        let actual_arrangements = solve(&record.springs, &record.damaged_spring_groups, 0, "");
         dbg!((input, actual_arrangements));
         assert_eq!(
             expected_arrangements, actual_arrangements,
@@ -100,20 +76,135 @@ impl Record {
     }
 }
 
+fn replace_first_char(s: &str, c: char) -> String {
+    // Convert the String to a Vec<char> to manipulate individual characters
+    let mut chars: Vec<char> = s.chars().collect();
+
+    // Replace the first character
+    if !chars.is_empty() {
+        chars[0] = c;
+    }
+
+    // Convert the Vec<char> back to a String
+    chars.into_iter().collect()
+}
+
+fn print_valid(indent: &str, thrown_away: &str, record: &str) {
+    debug_print!("{}* Valid: \"{}{}\"", indent, thrown_away, record);
+}
+fn print_invalid(indent: &str, thrown_away: &str, record: &str) {
+    debug_print!("{}* Invalid: \"{}{}\"", indent, thrown_away, record);
+}
+
+/// #Bried Take in a record and a vector of groups of damaged strings, and return the amount of different valid records that are possible.
+/// # Parameters
+/// - record: e.g. `????.######..#####.`
+/// - groups: e.g. `vec![1,6,5]`
+/// # Returns
+/// - Int: e.g. 4
+///
+/// # Example
+/// We will use dynamic programming to solve this problem.
+/// For example:
+/// ```rust
+///     solve(`????.######..#####.`, vec![1,6,5])
+///     ==
+///     solve(`????`, vec![1]) *
+///     solve(`######`, vec![6]) *
+///     solve(`#####`, vec![5])
+///     ==
+///     4 * 1 * 1
+///     ==
+///     4
+///```
+#[cfg(not(feature = "print_enabled"))]
+fn solve(record: &str, groups: &Vec<Int>, depth: usize, thrown_away: &str) -> Int {
+    if depth == 0 && !record.ends_with(".") {
+        return solve(&format!("{}.", record), groups, depth, thrown_away);
+    }
+    let indent = "-".repeat(depth);
+    if DEBUG || depth == 0 {
+        println!(
+            "{}* solve(\"{}\", \"{}\", {:?}, {})",
+            indent, thrown_away, record, groups, depth
+        );
+    }
+    let groups_sum: Int = groups.iter().sum();
+    if groups_sum >= record.len() as Int {
+        print_invalid(&indent, thrown_away, record);
+        return 0;
+    }
+    if groups.is_empty() {
+        if record.chars().all(|c| c == '.' || c == '?') || record.is_empty() {
+            print_valid(&indent, thrown_away, record);
+            return 1;
+        }
+        print_invalid(&indent, thrown_away, record);
+        return 0;
+    } else if record.is_empty() {
+        print_invalid(&indent, thrown_away, record);
+        return 0;
+    }
+    let depth = depth + 1;
+
+    for (i, c) in record.chars().enumerate() {
+        if i as Int > *groups.first().unwrap() {
+            print_invalid(&indent, thrown_away, record);
+            return 0;
+        }
+        match c {
+            '.' => {
+                if i == 0 {
+                    return solve(
+                        &record[1..],
+                        groups,
+                        depth,
+                        &format!("{}{}", thrown_away, &record[..1]),
+                    );
+                } else if i as Int == *groups.first().unwrap() {
+                    debug_print!("{}* Removed group of '{}'.", indent, i);
+                    return solve(
+                        &record[i..],
+                        &groups.iter().skip(1).cloned().collect(),
+                        depth,
+                        &format!("{}{}", thrown_away, &record[..i]),
+                    );
+                } else {
+                    print_invalid(&indent, thrown_away, record);
+                    return 0;
+                }
+            }
+            '#' => {
+                continue;
+            }
+            '?' => {
+                let a = record.replacen("?", ".", 1);
+                let b = record.replacen("?", "#", 1);
+                return solve(&a, groups, depth, thrown_away)
+                    + solve(&b, groups, depth, thrown_away);
+            }
+            _ => panic!("Invalid char '{}' in record.", c),
+        };
+    }
+    print_valid(&indent, thrown_away, record);
+    1
+}
+
 fn main() {
     // Part 1 - Example
-    assert!(Record::valid(&"#.#.###", &vec![1, 1, 3]));
-    assert!(Record::valid(&".###..##.#..", &vec![3, 2, 1]));
-    let example_input = vec![
+    //assert!(Record::valid(&"#.#.###", &vec![1, 1, 3]));
+    //assert!(Record::valid(&".###..##.#..", &vec![3, 2, 1]));
+    let sum: Int = vec![
         "???.### 1,1,3",
         ".??..??...?##. 1,1,3",
         "?#?#?#?#?#?#?#? 1,3,1,6",
         "????.#...#... 4,1,1",
         "????.######..#####. 1,6,5",
         "?###???????? 3,2,1",
-    ];
-    let sum: Int =
-        Record::sum_arrangements(example_input.iter().map(|s| s.to_string()).collect(), false);
+    ]
+    .iter()
+    .map(|s| Record::from_string(s).solve())
+    .sum();
     dbg!(sum);
     assert_eq!(21, sum, "This example value is always equal to 21.");
 
@@ -125,14 +216,32 @@ fn main() {
     Record::test("?###???????? 3,2,1", 10);
 
     // Part 2 - Example
-    //let sum: Int =
-    //Record::sum_arrangements(example_input.iter().map(|s| s.to_string()).collect(), true);
-    //dbg!(sum);
+    Record::test_expanded("???.### 1,1,3", 1);
+    Record::test_expanded(".??..??...?##. 1,1,3", 16384);
+    Record::test_expanded("?#?#?#?#?#?#?#? 1,3,1,6", 1);
+    Record::test_expanded("????.#...#... 4,1,1", 16);
+    Record::test_expanded("????.######..#####. 1,6,5", 2500);
+    Record::test_expanded("?###???????? 3,2,1", 506250);
 
     // Part 1
     if RUN_PART_1 {
-        let sum = Record::sum_arrangements(aoc_input::get(2023, 12), false);
+        let sum: Int = aoc_input::get(2023, 12)
+            .iter()
+            .filter(|s| !s.is_empty())
+            .map(|s| Record::from_string(s).solve())
+            .sum();
         dbg!(sum);
         assert_eq!(6935, sum, "This AOC value is always equal to 6935 for me.")
     }
+
+    // Part 2
+    if RUN_PART_2 {
+        let sum: Int = aoc_input::get(2023, 12)
+            .iter()
+            .filter(|s| !s.is_empty())
+            .map(|s| Record::from_string(s).expand().solve())
+            .sum();
+        dbg!(sum);
+    }
+    // assert_eq!(6935, sum, "This AOC value is always equal to 6935 for me.")
 }

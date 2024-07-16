cd097d7871e354be28bea4c31ab877e19df6d8ff
Jort van Waes <jortvanwaes@protonmail.com>
2024-07-16 19:55:17 +0200

    d15p2 - Example

--- src/day15.rs
+++ src/day15.rs
@@ -1,7 +1,147 @@
 mod test;
-use test::*;
+use std::fmt::Debug;
+
 type Int = i32;
 type InitializationSequence = Vec<Step>;
+
+struct Lens {
+    label: String,
+    focal_length: Int,
+    box_number: Int,
+}
+
+impl Debug for Lens {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[{} {}]", self.label, self.focal_length)
+    }
+}
+
+impl Lens {
+    fn from_string(step: &str) -> Lens {
+        let mut command = step.split("=");
+        let label = command.next().unwrap().to_string();
+        let focal_length: Int = command.next().unwrap().parse().unwrap();
+        let box_number = Step::get_hash_value(&label);
+        Lens {
+            label,
+            focal_length,
+            box_number,
+        }
+    }
+}
+
+struct Box {
+    box_number: Int,
+    lenses: Vec<Lens>,
+}
+
+impl Debug for Box {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if !self.lenses.is_empty() {
+            write!(
+                f,
+                "Box {}: {}",
+                self.box_number,
+                self.lenses
+                    .iter()
+                    .map(|l| format!("{:?}", l))
+                    .collect::<Vec<_>>()
+                    .join(" ")
+            )
+        } else {
+            write!(f, "")
+        }
+    }
+}
+
+impl Box {
+    fn get_lens_position(&self, label: &str) -> Option<usize> {
+        self.lenses.iter().position(|l| l.label == label)
+    }
+
+    fn get_focusing_power(&self) -> Int {
+        let focusing_power_from_box = 1 + self.box_number;
+        self.lenses
+            .iter()
+            .enumerate()
+            .map(|(i, l)| focusing_power_from_box * (i + 1) as Int * l.focal_length)
+            .sum()
+    }
+}
+
+struct BoxSequence {
+    boxes: Vec<Box>,
+}
+
+impl Debug for BoxSequence {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            self.boxes
+                .iter()
+                .filter(|b| !b.lenses.is_empty())
+                .map(|b| format!("{:?}", b))
+                .collect::<Vec<_>>()
+                .join("\n")
+        )
+    }
+}
+
+impl BoxSequence {
+    fn new() -> BoxSequence {
+        static NUMBER_OF_BOXES: Int = 256;
+        let mut boxes: Vec<Box> = Vec::new();
+        for i in 0..NUMBER_OF_BOXES {
+            boxes.push(Box {
+                box_number: i,
+                lenses: Vec::new(),
+            });
+        }
+        BoxSequence { boxes }
+    }
+
+    fn get_total_focussing_power(&self) -> Int {
+        self.boxes.iter().map(|b| b.get_focusing_power()).sum()
+    }
+
+    fn execute(&mut self, step: &Step) {
+        if step.to_string.contains("-") {
+            self.remove_lens(step);
+        } else if step.to_string.contains("=") {
+            self.insert_lens(step);
+        } else {
+            panic!("Invalid step: '{:?}'.", step);
+        }
+        println!("After {:?}:", step.to_string);
+        println!("{:?}\n", self);
+    }
+
+    fn remove_lens(&mut self, step: &Step) {
+        assert!(step.to_string.contains("-"));
+        let label = step.to_string.split("-").next().unwrap();
+        let box_number = Step::get_hash_value(&label);
+        let current_box = &mut self.get_box(box_number);
+        if let Some(position_of_lens_with_label) = current_box.get_lens_position(&label) {
+            current_box.lenses.remove(position_of_lens_with_label);
+        }
+    }
+
+    fn insert_lens(&mut self, step: &Step) {
+        assert!(step.to_string.contains("="));
+        let lens = Lens::from_string(&step.to_string);
+        let current_box = &mut self.get_box(lens.box_number);
+        if let Some(position_of_lens_with_label) = current_box.get_lens_position(&lens.label) {
+            current_box.lenses[position_of_lens_with_label] = lens;
+        } else {
+            current_box.lenses.push(lens);
+        }
+    }
+
+    fn get_box(&mut self, index: Int) -> &mut Box {
+        &mut self.boxes[index as usize]
+    }
+}
 
 #[derive(Debug)]
 struct Step {
@@ -73,20 +213,12 @@ fn main() {
         ("ot=7", 231),
     ];
     for (input, hash_value) in test_cases {
-        test_and_debug(
-            &hash_value,
-            &Step::from_string_slice(input).hash_value,
-            &format!("({:?}, {})", input, hash_value),
-        );
+        test!(hash_value, Step::from_string_slice(input).hash_value, input);
     }
     let example_sequence_input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
     let example_sequence = Step::from_string_slice_list(&example_sequence_input);
     let example_sequence_sum = sum_steps(&example_sequence);
-    test_and_debug(
-        &1320,
-        &example_sequence_sum,
-        &format!("({:?}, {})", example_sequence_input, example_sequence_sum),
-    );
+    test!(1320, example_sequence_sum);
 
     // Part 1
     let input: String = aoc_input::get(2023, 15)
@@ -96,9 +228,13 @@ fn main() {
         .collect();
     let sequence = Step::from_string_list(&input);
     let steps_sum = sum_steps(&sequence);
-    test_and_debug(
-        &507769,
-        &steps_sum,
-        &format!("Part 1: steps_sum == {}", steps_sum),
-    );
+    test!(507769, steps_sum, "Part 1: steps_sum == {}", steps_sum);
+
+    // Part 2 - Example
+    let mut example_box_sequence = BoxSequence::new();
+    example_sequence
+        .iter()
+        .for_each(|s| example_box_sequence.execute(&s));
+    let example_total_focussing_power = example_box_sequence.get_total_focussing_power();
+    test!(145, example_total_focussing_power);
 }
--- src/test.rs
+++ src/test.rs
@@ -1,16 +1,22 @@
-pub fn test<T: std::cmp::PartialEq + std::fmt::Debug>(expected: &T, actual: &T) {
-    assert_eq!(
-        expected, actual,
-        "Test case failed: this value should always equal '{:?}'.",
-        expected
-    );
-}
+#[macro_export]
+macro_rules! test {
+    ($expected:expr, $actual:expr) => {
+        test!($expected, $actual, stringify!($actual));
+    };
 
-pub fn test_and_debug<T: std::cmp::PartialEq + std::fmt::Debug>(
-    expected: &T,
-    actual: &T,
-    message: &str,
-) {
-    test(expected, actual);
-    println!("[Test Succeeded] {}", message);
+    // Match arm with format string parameter
+    ($expected:expr, $actual:expr, $fmt:expr, $($arg:tt)*) => {
+        let name = format!($fmt, $($arg)*);
+        test!($expected, $actual, name);
+    };
+
+    ($expected:expr, $actual:expr, $name:expr) => {
+        assert_eq!(
+            $expected, $actual,
+            "[Test Case] ❌ ({:?}, {:?})",
+            $expected, $name
+        );
+        println!("[Test Case] ✅ ({:?}, {:?})", $expected, $name);
+    };
+
 }

fee7869d20fea81fbcc1ccfd5f077e64d80ef4e4
Jort van Waes <jortvanwaes@protonmail.com>
2024-07-19 15:35:29 +0200

    fix: debugging Github workflow

--- src/aoc_input.rs
+++ src/aoc_input.rs
@@ -1,6 +1,6 @@
 pub fn get(year: i32, day: u32) -> Vec<String> {
     let aoc_json_cache_path = format!(".aocf/cache/aoc{}_{}.json", year, day);
-    let aoc_json_cache = aocf::Aoc::load_json_from(aoc_json_cache_path);
+    let aoc_json_cache = aocf::Aoc::load_json_from(&aoc_json_cache_path);
     let aoc_input = if aoc_json_cache.is_ok() {
         aoc_json_cache
             .unwrap()
@@ -9,6 +9,7 @@ pub fn get(year: i32, day: u32) -> Vec<String> {
             .get_input(false)
             .unwrap()
     } else {
+        panic!("No cache file found: {}", aoc_json_cache_path);
         aocf::Aoc::new()
             .year(Some(year))
             .day(Some(day))

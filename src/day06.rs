7650b5b9e5e0ba973932224d00a4e0169ba99729
Jort van Waes <jortvanwaes@protonmail.com>
2024-08-10 23:28:31 +0200

    refactor: Implemented Problem day06

--- src/day06.rs
+++ src/day06.rs
@@ -1,4 +1,5 @@
-mod macros;
+mod problem;
+use problem::*;
 
 type Uint = u64;
 
@@ -26,55 +27,76 @@ impl Race {
     }
 }
 
-fn main() {
-    println!("AOC src/day06.rs");
-    let input: Vec<String> = aoc::get(2023, 6);
-    let times = input.get(0).unwrap().split_ascii_whitespace().skip(1);
-    let distances = input.get(1).unwrap().split_ascii_whitespace().skip(1);
-    let time_distance_tuples = times.zip(distances);
+struct DaySix {}
 
-    let mut races: Vec<Race> = Vec::new();
+impl Problem for DaySix {
+    const YEAR: Year = 2023;
+    const DAY: Day = 6;
+    const PART_ONE_EXAMPLE_EXPECTED: Answer = 288;
+    const PART_ONE_EXPECTED: Answer = 160816;
+    const PART_TWO_EXAMPLE_EXPECTED: Answer = 71503;
+    const PART_TWO_EXPECTED: Answer = 46561107;
 
-    for (time, distance) in time_distance_tuples {
-        println!("({}, {})", time, distance);
-        let mut race = Race {
-            time: time.to_string().parse::<Uint>().unwrap(),
-            distance: distance.to_string().parse::<Uint>().unwrap(),
+    fn example_input() -> ExampleInput {
+        "
+        Time:      7  15   30
+        Distance:  9  40  200
+        "
+    }
+
+    fn solve_part_one(input: Input, is_example: bool) -> Answer {
+        let input: Vec<String> = input.lines().map(|s| s.to_string()).collect();
+        let times = input.get(0).unwrap().split_ascii_whitespace().skip(1);
+        let distances = input.get(1).unwrap().split_ascii_whitespace().skip(1);
+        let time_distance_tuples = times.zip(distances);
+
+        let mut races: Vec<Race> = Vec::new();
+
+        for (time, distance) in time_distance_tuples {
+            println!("({}, {})", time, distance);
+            let mut race = Race {
+                time: time.to_string().parse::<Uint>().unwrap(),
+                distance: distance.to_string().parse::<Uint>().unwrap(),
+                wins: Vec::new(),
+            };
+            race.generate_wins();
+            races.push(race);
+        }
+        debug!(is_example, &races);
+        let mut number_of_ways_to_beat_record = 0;
+        for race in &races {
+            if race.wins.len() > 0 && number_of_ways_to_beat_record == 0 {
+                number_of_ways_to_beat_record = 1;
+            }
+            number_of_ways_to_beat_record *= race.wins.len();
+        }
+
+        number_of_ways_to_beat_record as Answer
+    }
+
+    fn solve_part_two(input: Input, _is_example: bool) -> Answer {
+        let input: Vec<String> = input.lines().map(|s| s.to_string()).collect();
+        let convert_to_number = |s: &String| -> Uint {
+            s.split_ascii_whitespace()
+                .skip(1)
+                .collect::<Vec<&str>>()
+                .join("")
+                .to_string()
+                .parse::<Uint>()
+                .unwrap()
+        };
+        let time = convert_to_number(input.get(0).unwrap());
+        let distance = convert_to_number(input.get(1).unwrap());
+
+        let mut race: Race = Race {
+            time,
+            distance,
             wins: Vec::new(),
         };
         race.generate_wins();
-        races.push(race);
+        let number_of_ways_to_beat_second_record = race.wins.len();
+        number_of_ways_to_beat_second_record as Answer
     }
-    dbg!(&races);
-    let mut number_of_ways_to_beat_record = 0;
-    for race in &races {
-        if race.wins.len() > 0 && number_of_ways_to_beat_record == 0 {
-            number_of_ways_to_beat_record = 1;
-        }
-        number_of_ways_to_beat_record *= race.wins.len();
-    }
-    // Part 1
-    test!(160816, number_of_ways_to_beat_record);
-
-    //Part 2
-    let convert_to_number = |s: &String| -> Uint {
-        s.split_ascii_whitespace()
-            .skip(1)
-            .collect::<Vec<&str>>()
-            .join("")
-            .to_string()
-            .parse::<Uint>()
-            .unwrap()
-    };
-    let time = convert_to_number(input.get(0).unwrap());
-    let distance = convert_to_number(input.get(1).unwrap());
-
-    let mut race: Race = Race {
-        time,
-        distance,
-        wins: Vec::new(),
-    };
-    race.generate_wins();
-    let number_of_ways_to_beat_second_record = race.wins.len();
-    test!(46561107, number_of_ways_to_beat_second_record);
 }
+
+run!(DaySix);
